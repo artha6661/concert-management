21ee60b0eec67fa6418139ae0ea002cd5a6b3a8b
evgarden69 <arthabastanta03@gmail.com>
2026-04-16 22:22:48 +0700

    First merge

---
+++ src/test/java/com/edts/concert_ticket_reservation/ConcertTicketReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edts.concert_ticket_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertTicketReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
